1245fb1036a8a1c74c249e68b90ee06695c302b1
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2021-11-04 15:50:36 +0000

    sunfish: SuperiorOS bringup
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I9b9dd88a20c80bf6beef564a6dbac576ca0bf322

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sunfish.mk \
-    $(LOCAL_DIR)/aosp_sunfish_hwasan.mk \
+    $(LOCAL_DIR)/superior_sunfish.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sunfish-userdebug \
+    superior_sunfish-userdebug \
+    superior_sunfish-eng
